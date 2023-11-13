25449690b430562cab7d656cefef72a0942ceac6
lkwslr <git@lkwslr.de>
2023-11-13 21:09:59 +0100

    added basic iOS HausaufgabenWidget

---
+++ ios/Hausaufgaben/Hausaufgaben.swift
@@ -0,0 +1,80 @@
+//
+//  Hausaufgaben.swift
+//  Hausaufgaben
+//
+//  Created by lkwslr on 13.11.23.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), emoji: "😀")
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, emoji: "😀")
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let emoji: String
+}
+
+struct HausaufgabenEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            Text("Time:")
+            Text(entry.date, style: .time)
+
+            Text("Emoji:")
+            Text(entry.emoji)
+        }
+    }
+}
+
+struct Hausaufgaben: Widget {
+    let kind: String = "Hausaufgaben"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(iOS 17.0, *) {
+                HausaufgabenEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+            } else {
+                HausaufgabenEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    Hausaufgaben()
+} timeline: {
+    SimpleEntry(date: .now, emoji: "😀")
+    SimpleEntry(date: .now, emoji: "🤩")
+}
---
+++ ios/Hausaufgaben/HausaufgabenBundle.swift
@@ -0,0 +1,16 @@
+//
+//  HausaufgabenBundle.swift
+//  Hausaufgaben
+//
+//  Created by lkwslr on 13.11.23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HausaufgabenBundle: WidgetBundle {
+    var body: some Widget {
+        Hausaufgaben()
+    }
+}
